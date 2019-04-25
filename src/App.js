26c333b84199a9b2442ab6c09264b70c03bb8e92
olya belykh <belykh.o.v@gmail.com>
2019-04-25 17:54:57 +0300

    add auth component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
+import Auth from './components/auth'
+
 function App() {
   return (
     <div>
       <h1>Redux-Saga Concepts</h1>
+      <Auth />
     </div>
   );
 }
---
+++ src/components/auth/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+const Auth = class extends Component {
+
+    render() {
+        return(
+            <div>
+                <h2>Auth</h2>
+            </div>
+        )
+    }
+}
+
+export default Auth
