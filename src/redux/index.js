db1c661a7f31371d5875bcdb91821e92f2597395
olya belykh <belykh.o.v@gmail.com>
2019-04-25 21:14:59 +0300

    add logger

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,11 +1,12 @@
 import { createStore, applyMiddleware } from 'redux'
 import createSagaMiddleware from 'redux-saga'
+import logger from 'redux-logger'
 
 import reducer from './reducer'
 import saga from './saga'
 
 const sagaMiddleware = createSagaMiddleware()
-const enhancer = applyMiddleware(sagaMiddleware)
+const enhancer = applyMiddleware(logger, sagaMiddleware)
 
 const store = createStore(reducer, enhancer)
 export default store
