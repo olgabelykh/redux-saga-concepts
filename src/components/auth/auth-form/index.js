31b4d05506955f9decec7366ce1a01a5214ffad5
olya belykh <belykh.o.v@gmail.com>
2019-04-25 18:59:31 +0300

    add auth, auth-form components

---
+++ src/components/auth/auth-form/index.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+
+const AuthForm = props => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const emailChangeHandler = event => setEmail(event.target.value)
+    const passwordChangeHandler = event => setPassword(event.target.value)
+
+    const { submit, caption } = props
+
+    const submitHandler = event => {
+        event.preventDefault()
+        submit(email, password)
+    }
+
+    return (
+        <form onSubmit={submitHandler}>
+            <input
+                type="email"
+                name="email"
+                placeholder="email"
+                value={email}
+                onChange={emailChangeHandler} />
+            <input
+                type="password"
+                name="password"
+                placeholder="password"
+                value={password}
+                onChange={passwordChangeHandler}/>
+            <button type ="submit">{caption}</button>
+        </form>
+    )
+}
+
+export default AuthForm
