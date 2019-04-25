03e60bbed1954437df3718150e9ead1d1566b517
olya belykh <belykh.o.v@gmail.com>
2019-04-25 19:05:28 +0300

    add auth-form

--- src/components/auth/index.js
+++ src/components/auth/index.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
 
+import AuthForm from './auth-form'
+
 const Auth = class extends Component {
 
     render() {
         return(
             <div>
                 <h2>Auth</h2>
+                <AuthForm
+                    submit={(email, password) => console.log('Sign Up', email, password)}
+                    caption="Sign Up" />
+                <AuthForm
+                    submit={(email, password) => console.log('Sign In', email, password)}
+                    caption="Sign In" />
             </div>
         )
     }
